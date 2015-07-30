c4016e95ba0aa93f620bce24eccf219e8e2fb5e8
Greg Laabs <OverloadUT@gmail.com>
2015-07-30 16:28:48 -0700

    Starting work on character and npc cards

--- arrpg-tools.js
+++ arrpg-tools.js
@@ -2,19 +2,6 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-
   Template.arrpg_characters.helpers({
     characters: [
       {name: "Alice", aspects: [
@@ -23,6 +10,56 @@ if (Meteor.isClient) {
         {aspectname: "Subtlety is for bitches"},
         {aspectname: "But I’m street smart!"}
       ]},
+      // {name: "Window", aspects: [
+      //   {aspectname: "Ranged Weapons Specialist"},
+      //   {aspectname: "Moon-man of action"},
+      //   {aspectname: "Kind words"},
+      //   {aspectname: "No half-measures"},
+      //   {aspectname: "Fear of physiology"}
+      // ]},
+      {name: "Jaguar Mirror", aspects: [
+        {aspectname: "Smooth-Talking Scoundrel"},
+        {aspectname: "The less you know..."},
+        {aspectname: "Let's just talk about this"},
+        {aspectname: "Sometimes words aren't enough"},
+        {aspectname: "... with a conscience"}
+      ]},
+      {name: "Steve", aspects: [
+        {aspectname: "Nohaine Magic Monk with Water Symbiote"},
+        {aspectname: "My brain is my body, my body is everything"},
+        {aspectname: "People rely on technology too much"},
+        {aspectname: "I’ve been doing this for the better part of the last century"},
+        {aspectname: "Socially inept"}
+      ]},
+      {name: "Zadia", aspects: [
+        {aspectname: "Pirate Diplomat"},
+        {aspectname: "Nothing works like a rousing speech"},
+        {aspectname: "We are all pieces in the Great Game"},
+        {aspectname: "Kicking butts and making friends"},
+        {aspectname: "Why shouldn’t I be in charge?"}
+      ]},
+      {name: "Clint", aspects: [
+        {aspectname: "Ex-con Conspiracy Theorist"},
+        {aspectname: "Unburdened by scruples"},
+        {aspectname: "Gun-nut"},
+        {aspectname: "DON’T LET THEM GET TAKEN"},
+        {aspectname: "A danger to himself and others"}
+      ]}
+    ]
+  });
+
+  Template.arrpg_npcs.helpers({
+    characters: [
+      {name: "M12 Infantry (Nameless NPC)", aspects: [
+        {aspectname: "Well-Armed Soldier"},
+        {aspectname: "Tactical Maneuvers"}
+      ], modes: [
+        {
+          modename: "Action",
+          moderank: "Good",
+          modevalue: "+3"
+        }
+      ]}
     ]
   });
 }
